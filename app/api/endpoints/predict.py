90be21e5dea178157471c13daed7ac28f28886f4
Fardin Haque <fardinhaque60@gmail.com>
2025-07-28 18:28:19 -0700

    updated endpoints to retrieve data for each garage

--- app/api/endpoints/predict.py
+++ app/api/endpoints/predict.py
@@ -1,6 +1,5 @@
-# TODO update endpoints to stream responses
 from fastapi import APIRouter
-from app.models.schemas import PredictionResponse, AllPredictionsResponse
+from app.models.schemas import PredictionResponse, AllModelsPredictionResponse, QuickPredictionResponse
 from app.utils.lib import get_minutes_from_week_start
 from app.ml_models.load_models import get_model_helper
 import logging
@@ -14,34 +13,60 @@ router = APIRouter()
 model_helper = get_model_helper()
 MODELS = model_helper.available_models
 
+# PRODUCTION endpoint to make prediction for all garages
 @router.get("/predict", response_model=PredictionResponse)
-def predict(timestamp: datetime, garage: str):
-    logger.info(f"params for predict request: timestamp={timestamp}, garage={garage}")
+def predict(timestamp: datetime):
+    logger.info(f"params for predict request: timestamp={timestamp}")
 
     # Get minutes from week start
     minutes = get_minutes_from_week_start(timestamp)
     
     # Make prediction
     start_time = datetime.now()
-    prediction = model_helper.production_model(minutes, garage)
+    predictions = model_helper.production_model(minutes)
     end_time = datetime.now()
-    logger.info(f"prediction: {prediction}")
+    logger.info(f"prediction: {predictions}")
     
     # return prediction object
-    prediction_time = int((end_time - start_time).total_seconds() * 1000)
+    prediction_time = round((end_time - start_time).total_seconds(), 4)
     prediction_record = {
         "timestamp": timestamp,
-        "garage": garage,
-        "prediction": prediction,
+        "predictions": predictions,
         "prediction_time": prediction_time
     }
     
     return prediction_record
 
-# returns predictions for all models
-@router.get("/predict_all", response_model=AllPredictionsResponse)
-def predict_all(timestamp: datetime, garage: str):
-    logger.info(f"params for predict request: timestamp={timestamp}, garage={garage}")
+# PRODUCTION endpoint to return predictions for the next 30 mins, 1 hour, and 2 hours
+@router.get("/quick_predict", response_model=QuickPredictionResponse)
+def quick_predict(timestamp: datetime):
+    logger.info(f"params for quick_predict request: timestamp={timestamp}")
+
+    # Get minutes from week start
+    minutes = get_minutes_from_week_start(timestamp)
+    
+    # Make predictions
+    start_time = datetime.now()
+    predictions = {}
+    for interval in [30, 60, 120]:  # 30 mins, 1 hour, 2 hours    
+        predictions[f"next_{interval}_mins"] = model_helper.production_model(minutes + interval)
+    end_time = datetime.now()
+    logger.info(f"predictions: {predictions}")
+    
+    # return prediction object
+    prediction_time = round((end_time - start_time).total_seconds(), 4)
+    prediction_record = {
+        "timestamp": timestamp,
+        "predictions": predictions,
+        "prediction_time": prediction_time
+    }
+    
+    return prediction_record
+
+# GENERAL endpoint that returns predictions from all models
+@router.get("/predict_all", response_model=AllModelsPredictionResponse)
+def predict_all(timestamp: datetime):
+    logger.info(f"params for predict_all request: timestamp={timestamp}")
 
     # Get minutes from week start
     minutes = get_minutes_from_week_start(timestamp)
@@ -50,16 +75,15 @@ def predict_all(timestamp: datetime, garage: str):
     predictions = {}
     start_time = datetime.now()
     for model in MODELS:
-        predictions[model] = model_helper.ml_model(model, minutes, garage)
+        predictions[model] = model_helper.ml_model(model, minutes)
     end_time = datetime.now()
     logger.info(f"predictions: {predictions}")
     
     # return prediction object
-    prediction_time = int((end_time - start_time).total_seconds() * 1000)
+    prediction_time = round((end_time - start_time).total_seconds(), 4)
     prediction_record = {
         "timestamp": timestamp,
-        "garage": garage,
-        "predictions_list": predictions,
+        "predictions": predictions,
         "prediction_time": prediction_time
     }
     
--- app/models/schemas.py
+++ app/models/schemas.py
@@ -1,14 +1,22 @@
 from pydantic import BaseModel
 from datetime import datetime
 
-class PredictionResponse(BaseModel):
+class QuickPredictions(BaseModel):
+    next_30_mins: dict  # predictions from all garages for next 30 mins
+    next_60_mins: dict 
+    next_120_mins: dict  
+
+class QuickPredictionResponse(BaseModel):
     timestamp: datetime  # same timestamp as in request
-    garage: str   # same garage as in request
-    prediction: float  # predicted fullness percentage
+    predictions: QuickPredictions  # predictions for next 30 mins, 1 hour, and 2 hours
     prediction_time: float  # time in secs taken to make prediction
 
-class AllPredictionsResponse(BaseModel):
+class PredictionResponse(BaseModel):
+    timestamp: datetime  
+    predictions: dict  
+    prediction_time: float  
+
+class AllModelsPredictionResponse(BaseModel):
     timestamp: datetime
-    garage: str   
-    predictions_list: dict  # list of predictions from all models
-    prediction_time: float 
+    predictions: dict  # every garage prediction for all models
+    prediction_time: float
