0c3f5a89011055241ac431f5dfbc44c16afe8d49
Fardin Haque <fardinhaque60@gmail.com>
2025-08-03 17:56:06 -0700

    update cors

--- app/main.py
+++ app/main.py
@@ -1,6 +1,20 @@
 from fastapi import FastAPI
 from app.api.endpoints import predict
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI(title="ParkPredict API")
 
 app.include_router(predict.router, prefix="/api")
+
+origins = [
+    "http://localhost:5173",  
+    "https://sjsuparkpredict.vercel.app",  
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],  # Only allows GET requests
+    allow_headers=["application/json"],  # Only allows 'application/json' header
+)
